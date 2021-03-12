df359eb26fcda8bdc5844f6ab1e113428569e0db
n2dev <n2dev.code@gmail.com>
2021-03-12 10:35:59 +0100

    ADD ProductsVC

--- GNBank/ViewController.swift
+++ GNBank/ViewController.swift
@@ -14,6 +14,10 @@ class ViewController: UIViewController {
     // Do any additional setup after loading the view.
   }
 
-
+  @IBAction func clickShowProducts(_ sender: Any) {
+    let productsVC = ProductsViewController()
+    self.present(productsVC, animated: true)
+  }
+  
 }
 

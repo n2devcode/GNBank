b9c68c67533cc8f7fbd0e63dbb4eaf65b76a9418
n2dev <n2dev.code@gmail.com>
2021-03-12 10:12:37 +0100

    first commit

---
+++ GNBank/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GNBank
+//
+//  Created by Anna on 12/03/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
